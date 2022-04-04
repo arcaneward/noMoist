127162e47c77bd4bda510a1063befc21acc265c5
Lindsey <84740121+lin-dsey@users.noreply.github.com>
2022-04-04 17:03:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+replaceText(document.body)
+// recursive function that loops through every element until it finds the word "moist"
+function replaceText(element) {
+  // loops through child nodes & calls replaceMoist function for each child node
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText)
+  // if there are no children, check if in a text node
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    // modify text in node
+    if (element.textContent.match(/moist/gi)) {
+      const newElement = document.createElement('span')
+      newElement.innerHTML = element.textContent.replace(/(moist)/gi,
+      '<span style="background-color: black; color: black;">$1</span>')
+      element.replaceWith(newElement)
+    }
+  }
+}
